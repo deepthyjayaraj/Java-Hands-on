8c71dce6cba5ad3cbb234babd564f4921ffac74c
deepthyjayaraj <deepthyjayaraj@localhost>
2021-05-05 23:08:30 -0700

    b

---
+++ Java-Hands-On/src/Java/print/Variables.java
@@ -0,0 +1,17 @@
+package Java.print;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Declaring (Creating) Variables
+
+		String name = "Hello";
+		System.out.println(name);
+		String name2 = "World";
+		System.out.println(name2);
+
+	}
+
+}
---
+++ Java-Hands-On/src/Java/print/comment.java
@@ -0,0 +1,17 @@
+package Java.print;
+
+public class comment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+// to comment a line
+		
+		/* to
+		  comment
+		  multi-lines*/
+		 
+	}
+
+}
